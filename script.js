1b8259298637a9b763d761979c7bbf7f96705bd2
Andrii Anisimov <aanis@softserveinc.com>
2020-03-29 11:10:08 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Getting the User’s Position
+// navigator.geolocation.getCurrentPosition(position => {
+//     console.log(position.coords.latitude);
+//     console.log(position.coords.longitude);
+// });
+
+
+// Watching the position for changes
+// navigator.geolocation.watchPosition(position => {
+//     console.log(position);
+// });
+
+// const id = navigator.geolocation.watchPosition(position => {
+// 	console.log(position);
+// });
+
+// Stop watching after 10 seconds
+// setTimeout(() => {
+//     navigator.geolocation.clearWatch(id);
+// }, 10 * 1000);
+
+
+// If the user denies the position
+// navigator.geolocation.getCurrentPosition(position => {
+//     console.log(position);
+// }, error => {
+//     console.error(error);
+// });
